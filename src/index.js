0350a8d414d1934e8124ae59ca4a35eab6b58b10
DennisShin <dennisshin0@gmail.com>
2023-09-20 10:44:37 -0400

    Core Deliverables

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
 // write your code here
+
+function addRamen(ramen){
+    const ramenImg = document.createElement("img");
+    ramenImg.src = ramen.image;
+    ramenImg.addEventListener("click", (e) => {
+        console.log(ramen.id + " was clicked!")
+        //change ramen details
+        document.querySelector(".detail-image").src = ramen.image
+        document.querySelector(".name").textContent = ramen.name
+        document.querySelector(".restaurant").textContent = ramen.restaurant
+        document.querySelector("#rating-display").textContent = ramen.rating
+        document.querySelector("#comment-display").textContent = ramen.comment
+    })
+    document.querySelector("#ramen-menu").append(ramenImg);
+}
+
+fetch("http://localhost:3000/ramens")
+.then(res => res.json())
+.then(ramens => {
+    ramens.forEach(ramen => {
+       addRamen(ramen);
+    })
+})
+
+document.querySelector("#new-ramen").addEventListener("submit", (e) => {
+    e.preventDefault();
+    let newRam = {
+        "name": e.target.name.value,
+        "restaurant": e.target.restaurant.value,
+        "image": e.target.image.value,
+        "rating": e.target.rating.value,
+        "comment": e.target["new-comment"].value   
+    }
+
+    addRamen(newRam)
+    document.querySelector("#new-ramen").reset();
+})
